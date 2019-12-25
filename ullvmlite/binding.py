c54f0df076c23611824caaae96e43d58331ef584
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-12-25 13:15:55 +0300

    binding: Basic implementation of create_mcjit_compiler().

--- ullvmlite/binding.py
+++ ullvmlite/binding.py
@@ -15,3 +15,27 @@ def initialize_native_target():
 def initialize_native_asmprinter():
     # TODO: Fix hardcoding to x86
     LLVMInitializeX86AsmPrinter()
+
+
+class Engine:
+
+    def __init__(self, engine):
+        self.engine = engine
+
+    def __int__(self):
+        return self.engine
+
+    def add_module(self, mod):
+        LLVMAddModule(self.engine, mod)
+
+    def get_function_address(self, name):
+        return LLVMGetFunctionAddress(self.engine, name)
+
+
+def create_mcjit_compiler(mod, target_machine):
+    print("Warning: create_mcjit_compiler: target_machine is ignored")
+    engine_ref = by_ref("P")
+    errmsg_ref = by_ref("P")
+    res = LLVMCreateMCJITCompilerForModule(engine_ref, mod, 0, 0, errmsg_ref)
+    assert not res
+    return Engine(engine_ref[0])
